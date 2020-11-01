dd6e41fea990573c2466212d33aae8a4b78f45ca
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-01 20:56:22 +0530

    1. Added business logic for get, add todo API endpoints in API service. 2. Created functions in controller for handling GET, POST calls on /api/todos endpoint.

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+interface Todo {
+  title: string;
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  todos: Array<Todo> = new Array({ title: 'Todo 1' }, { title: 'Todo 2' });
+
+  getData(): Array<Todo> {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos = [...this.todos, { title: 'Todo 3' }];
   }
 }
