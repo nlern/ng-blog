0a08376d3c5f5bdbd8c72e9bdffdcc9a63d3b361
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-01 20:14:45 +0530

    1. Changed AppComponent code to pass tests.

--- apps/ng-blog/src/app/app.component.ts
+++ apps/ng-blog/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-blog';
+export class AppComponent implements OnInit {
+  todos: Array<Todo>;
+
+  ngOnInit() {
+    this.todos = new Array<Todo>(
+      {
+        title: 'Todo 1',
+      },
+      {
+        title: 'Todo 2',
+      }
+    );
+  }
+
+  addTodo() {
+    this.todos = [...this.todos, { title: 'Todo 3' }];
+  }
 }
