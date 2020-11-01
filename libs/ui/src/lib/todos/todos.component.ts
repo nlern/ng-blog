7ba441b82d875f60b10dea64d4b0176de1e00fd0
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-01 21:53:07 +0530

    1. Added logic to show todos in TodosComponent. 2. Updated TodosComponent .ts file to fix lint errors. 3. Added UiModule in AppModule to use TodosComponent in UI. 4. Refactored AppComponent html to use TodoComponent.

--- libs/ui/src/lib/todos/todos.component.ts
+++ libs/ui/src/lib/todos/todos.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '@myorg/data';
 
 @Component({
   selector: 'myorg-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodosComponent {
+  @Input() todos: Todo[];
 }
