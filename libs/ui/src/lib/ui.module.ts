a6c27a0f6c7ade514b4a996045bf79385b5c8760
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-02 11:05:35 +0530

    1. Created Shell, Navbar, Footer components in UI module. 2. Used these components in AppComponent. 3. Added bootstrap library to project.

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosComponent } from './todos/todos.component';
+import { ShellComponent } from './containers/shell/shell.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TodosComponent],
-  exports: [TodosComponent],
+  declarations: [
+    TodosComponent,
+    ShellComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListComponent,
+  ],
+  exports: [
+    TodosComponent,
+    ShellComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListComponent,
+  ],
 })
 export class UiModule {}
